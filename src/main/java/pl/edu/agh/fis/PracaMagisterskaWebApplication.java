39fafa38418e4735b4c0fdeb2e450625485b9048
Wemstar <sylwestermacura@gmail.com>
2015-09-04 18:24:00 +0200

    Nowa rozkład projektu

---
+++ src/main/java/pl/edu/agh/fis/PracaMagisterskaWebApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.fis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaMagisterskaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaMagisterskaWebApplication.class, args);
+    }
+}
